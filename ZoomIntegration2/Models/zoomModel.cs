d02acc059cf4f900ae32dcb8db6d4d4eba80c981
CodeLuminary <victorijoni@yahoo.com>
2021-09-05 21:34:51 +0100

    zoom model setup

---
+++ ZoomIntegration2/Models/zoomModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZoomIntegration2.Models
+{
+    public class zoomModel
+    {
+        public string topic { get; set; }
+        public int duration { get; set; }
+        public string hostvideo { get; set; }
+        public string parvideo { get; set; }
+        public string muteStud { get; set; }
+        public string allowstud { get; set; }
+        public string start_time { get; set; }
+    }
+}
